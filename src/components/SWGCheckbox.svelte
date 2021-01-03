e5360ae514e0bb7ccaab5eac2d2b3f52bd059553
CapitanPNG <pierniccologiannelli@gmail.com>
2021-01-03 18:56:52 +0100

    v0

--- src/components/SWGCheckbox.svelte
+++ src/components/SWGCheckbox.svelte
@@ -55,13 +55,72 @@ let callbacks = {
 $: if(input !== null) input.disabled = disabled;
 
 onMount(function(e) {
+    controller.getData = function (key) {
+        const messagePrefix = "\n\nCannot get data:\n";
+        let message = messagePrefix;
 
+        let properties = {
+            "disabled"  : disabled,
+            "checked"   : checked,
+            "label"     : label,
+
+            "value"     : value
+        };
+
+        if(key === undefined) {
+            return properties;
+        }
+
+        switch(key) {
+            case "disabled":
+            case "checked":
+            case "label":
+            case "value":
+                return properties[key];
+            break;
+            default:
+                message += "\nProperty '" + key + "' is not recognized";
+                message += "\n\n";
+
+                throw new Error(message);
+        }
+    };
+
+    controller.setData = function (
+        key,
+        val
+    ) {
+        const messagePrefix = "\n\nCannot set data:\n";
+        let message = messagePrefix;
+
+        switch(key) {
+            case "disabled":
+                disabled = val;
+            break;
+            case "checked":
+                checked = val;
+            break;
+            case "label":
+                label = val;
+            break;
+            case "value":
+                value = val;
+            break;
+            default:
+                message += "\nProperty '" + key + "' is not recognized";
+                message += "\n\n";
+
+                throw new Error(message);
+        }
+    };
 });
 
 </script>
 
 <label class="checkbox"
     bind:this={controller}
+
+    {disabled}
 >
     <div class="checkbox-body">
         <slot name="body"></slot>
@@ -135,24 +194,27 @@ input {
     color: #ffffff;
 }
 
-input:checked + .checkbox-footer .checkbox-emulator-box {
-    background-color: #00bd9c;
-}
-
 input:not(:checked) + .checkbox-footer .checkbox-emulator-box {
     background-color: #e7e7e7;
-}
-
-input:checked + .checkbox-footer .checkbox-emulator {
-    visibility: visible;
 }
 
 input:not(:checked) + .checkbox-footer .checkbox-emulator {
     visibility: hidden;
 }
 
-.checkbox-label {
+input:checked + .checkbox-footer .checkbox-emulator-box {
+    background-color: #00bd9c;
+}
 
+input:checked + .checkbox-footer .checkbox-emulator {
+    visibility: visible;
+}
+
+.checkbox-label {
+    margin-left: 10px;
+    font-weight: 600;
+    font-size: 14px;
+    color: #515151;
 }
 
 </style>
